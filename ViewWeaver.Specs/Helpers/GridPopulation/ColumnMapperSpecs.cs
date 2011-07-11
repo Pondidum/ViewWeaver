0bd3ec1f411f93377c047cb9d67ea35f9620e1f9
Pondidum <Pondidum@stormbase.net>
2011-07-11 12:58:31 +0100

    finished columnMapper specs

--- ViewWeaver.Specs/Helpers/GridPopulation/ColumnMapperSpecs.cs
+++ ViewWeaver.Specs/Helpers/GridPopulation/ColumnMapperSpecs.cs
@@ -7,51 +7,84 @@ using ViewWeaver.Helpers.GridPopulation;
 namespace ViewWeaver.Specs.Helpers.GridPopulation
 {
 
-	public class ColumMapperSpec<T> : SpecBase
+	public class ColumnMapperSpec<T> : SpecBase
 	{
 		protected static IList<ColumnMapping<T>> mapping;
 	}
 
-	public class When_passed_a_type_with_no_members : ColumMapperSpec<NoMembers>
+	public class When_passed_a_type_with_no_members : ColumnMapperSpec<NoMembers>
 	{
 		Because of = () => mapping =  ColumnMapper.Map<NoMembers>();
 		It should_return_no_mappings = () => mapping.ShouldBeEmpty();
 	}
 
-	public class When_passed_a_type_with_one_private_property : ColumMapperSpec<OnePrivateProperty>
+	public class When_passed_a_type_with_one_private_property : ColumnMapperSpec<OnePrivateProperty>
 	{
 		Because of = () => mapping = ColumnMapper.Map<OnePrivateProperty>();
 		It should_return_no_mappings = () => mapping.ShouldBeEmpty();
 	}
 
-	public class When_passed_a_type_with_one_public_method : ColumMapperSpec<OnePublicMethod>
+	public class When_passed_a_type_with_one_public_method : ColumnMapperSpec<OnePublicMethod>
 	{
 		Because of = () => mapping = ColumnMapper.Map<OnePublicMethod>();
 		It should_return_no_mappings = () => mapping.ShouldBeEmpty();
 	}
 
-	public class When_passed_a_type_with_one_public_write_only_property : ColumMapperSpec<OnePublicWriteonlyProperty>
+	public class When_passed_a_type_with_one_public_write_only_property : ColumnMapperSpec<OnePublicWriteonlyProperty>
 	{
 		Because of = () => mapping = ColumnMapper.Map<OnePublicWriteonlyProperty>();
 		It should_return_no_mappings = () => mapping.ShouldBeEmpty();
 	}
 
-	public class When_passed_a_type_with_one_public_readonly_property : ColumMapperSpec<OnePublicReadonlyProperty>
+	public class When_passed_a_type_with_one_public_readonly_property : ColumnMapperSpec<OnePublicReadonlyProperty>
 	{
 		Because of = () => mapping = ColumnMapper.Map<OnePublicReadonlyProperty>();
 		It should_return_one_mapping = () => mapping.Count.ShouldEqual(1);
 		It should_have_an_index_of_zero = () => mapping.First().Index.ShouldEqual(0);
 		It should_get_property_return_type = () => mapping.First().DataType.ShouldEqual(typeof (String));
 		It should_be_named_after_the_property = () => mapping.First().Name.ShouldEqual("Test");
+		It should_return_the_property_value = () => mapping.First().Populate(new OnePublicReadonlyProperty("omg")).ShouldEqual("omg");
+	}
+
+	public class When_passed_a_type_with_two_public_readonly_properties : ColumnMapperSpec<TwoPublicReadonlyProperties>
+	{
+		Because of = () => mapping = ColumnMapper.Map<TwoPublicReadonlyProperties>();
+		It should_return_two_mappings = () => mapping.Count.ShouldEqual(2);
 	}
 
 
 	public class NoMembers {}
-	public class OnePrivateProperty { private String Test { get; set; } }
-	public class OnePublicWriteonlyProperty { public String Test { private get; set; } }
-	public class OnePublicReadonlyProperty { public String Test { get; private set; } }
-	public class OnePublicProperty { public String Test { get; set; } }
-	public class OnePublicMethod { public String Test() { return "value"; } }
+	
+	public class OnePrivateProperty
+	{
+		private String Test { get; set; }
+	}
+	
+	public class OnePublicWriteonlyProperty
+	{
+		public String Test { private get; set; }
+	}
+	
+	public class OnePublicReadonlyProperty
+	{
+		public OnePublicReadonlyProperty(String value) { Test = value; }
+		public String Test { get; private set; }
+	}
+	
+	public class OnePublicProperty
+	{
+		public String Test { get; set; }
+	}
+	
+	public class OnePublicMethod
+	{
+		public String Test() { return "value"; }
+	}
 
+	public class TwoPublicReadonlyProperties 
+	{
+		public String FirstProperty { get; private set; }
+		public int SecondProperty { get; private set; }
+	}
 
 }
