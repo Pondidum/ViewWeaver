ece9b7f328ea41ae45ec4429a46b846905187575
Andy <=>
2011-12-13 22:12:14 +0000

    added forms testapp

---
+++ ViewWeaver.TestApp/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ViewWeaver.TestApp
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dgv = new System.Windows.Forms.DataGridView();
+			this.btnRegister = new System.Windows.Forms.Button();
+			this.btnPopulate = new System.Windows.Forms.Button();
+			this.btnSetupColumns = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dgv
+			// 
+			this.dgv.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgv.Location = new System.Drawing.Point(12, 12);
+			this.dgv.Name = "dgv";
+			this.dgv.Size = new System.Drawing.Size(260, 150);
+			this.dgv.TabIndex = 0;
+			// 
+			// btnRegister
+			// 
+			this.btnRegister.Location = new System.Drawing.Point(12, 168);
+			this.btnRegister.Name = "btnRegister";
+			this.btnRegister.Size = new System.Drawing.Size(75, 23);
+			this.btnRegister.TabIndex = 1;
+			this.btnRegister.Text = "Register";
+			this.btnRegister.UseVisualStyleBackColor = true;
+			this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+			// 
+			// btnPopulate
+			// 
+			this.btnPopulate.Location = new System.Drawing.Point(174, 168);
+			this.btnPopulate.Name = "btnPopulate";
+			this.btnPopulate.Size = new System.Drawing.Size(75, 23);
+			this.btnPopulate.TabIndex = 2;
+			this.btnPopulate.Text = "Populate";
+			this.btnPopulate.UseVisualStyleBackColor = true;
+			this.btnPopulate.Click += new System.EventHandler(this.btnPopulate_Click);
+			// 
+			// btnSetupColumns
+			// 
+			this.btnSetupColumns.Location = new System.Drawing.Point(93, 168);
+			this.btnSetupColumns.Name = "btnSetupColumns";
+			this.btnSetupColumns.Size = new System.Drawing.Size(75, 23);
+			this.btnSetupColumns.TabIndex = 3;
+			this.btnSetupColumns.Text = "Setup";
+			this.btnSetupColumns.UseVisualStyleBackColor = true;
+			this.btnSetupColumns.Click += new System.EventHandler(this.btnSetupColumns_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.btnSetupColumns);
+			this.Controls.Add(this.btnPopulate);
+			this.Controls.Add(this.btnRegister);
+			this.Controls.Add(this.dgv);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dgv;
+		private System.Windows.Forms.Button btnRegister;
+		private System.Windows.Forms.Button btnPopulate;
+		private System.Windows.Forms.Button btnSetupColumns;
+	}
+}
+
---
+++ ViewWeaver.TestApp/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ViewWeaver.Helpers.GridPopulation;
+using ViewWeaver.Helpers.GridPopulation.GridPopulators;
+
+namespace ViewWeaver.TestApp
+{
+	public partial class Form1 : Form
+	{
+		private ColumnMappingCollection<Product> _mapping;
+
+		public Form1()
+		{
+			InitializeComponent();
+			_mapping = ColumnMapper.AutomapForType<Product>();
+			_mapping = ColumnMapper.ForType<Product>(x => x.Named("test").AtIndex(3));
+		}
+
+		private void btnRegister_Click(object sender, EventArgs e)
+		{
+			GridHandler.AddPopulator(typeof(DataGridView), new DataGridViewPopulator());
+		}
+
+		private void btnSetupColumns_Click(object sender, EventArgs e)
+		{
+			GridHandler.ConfigureColumns(dgv, _mapping);
+		}
+
+		private void btnPopulate_Click(object sender, EventArgs e)
+		{
+			//var m2 = ColumnMapper.For<Product>().DefineColumn(new ColumnMapping<Product>());
+
+			//var config = GridHandler.CreateConfiguration<Product>(c => c.WithColumn(0, "Name", "Name", typeof(String), x => x.Name));
+
+			var collection = new List<Product>();
+			collection.Add(new Product());
+
+			GridHandler.Populate(dgv, _mapping, collection);
+		}
+
+	}
+
+	public class Product
+	{
+		public String Name { get; set; }
+
+		public Product()
+		{
+			Name = Guid.NewGuid().ToString();
+		}
+	}
+}
