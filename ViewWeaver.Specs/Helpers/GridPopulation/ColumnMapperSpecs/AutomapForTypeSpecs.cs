c4dc1ac3c8aa87d7fd2c9a5cd963711e88200917
Andy <=>
2011-12-17 00:49:34 +0000

    specs for columnMapper

--- ViewWeaver.Specs/Helpers/GridPopulation/ColumnMapperSpecs/AutomapForTypeSpecs.cs
+++ ViewWeaver.Specs/Helpers/GridPopulation/ColumnMapperSpecs/AutomapForTypeSpecs.cs
@@ -7,44 +7,47 @@ using ViewWeaver.Specs.TestData.Classes;
 
 namespace ViewWeaver.Specs.Helpers.GridPopulation.ColumnMapperSpecs
 {
-	public class When_passed_a_type_with_no_members : ColumnMapperSpecBase<NoMembers>
+	namespace AutomapForType
 	{
-		Because of = () => mapping =  ColumnMapper.AutomapForType<NoMembers>();
-		It should_return_no_mappings = () => mapping.ShouldBeEmpty();
-	}
+		public class When_passed_a_type_with_no_members : ColumnMapperSpecBase<NoMembers>
+		{
+			Because of = () => mapping = ColumnMapper.AutomapForType<NoMembers>();
+			It should_return_no_mappings = () => mapping.ShouldBeEmpty();
+		}
 
-	public class When_passed_a_type_with_one_private_property : ColumnMapperSpecBase<OnePrivateProperty>
-	{
-		Because of = () => mapping = ColumnMapper.AutomapForType<OnePrivateProperty>();
-		It should_return_no_mappings = () => mapping.ShouldBeEmpty();
-	}
+		public class When_passed_a_type_with_one_private_property : ColumnMapperSpecBase<OnePrivateProperty>
+		{
+			Because of = () => mapping = ColumnMapper.AutomapForType<OnePrivateProperty>();
+			It should_return_no_mappings = () => mapping.ShouldBeEmpty();
+		}
 
-	public class When_passed_a_type_with_one_public_method : ColumnMapperSpecBase<OnePublicMethod>
-	{
-		Because of = () => mapping = ColumnMapper.AutomapForType<OnePublicMethod>();
-		It should_return_no_mappings = () => mapping.ShouldBeEmpty();
-	}
+		public class When_passed_a_type_with_one_public_method : ColumnMapperSpecBase<OnePublicMethod>
+		{
+			Because of = () => mapping = ColumnMapper.AutomapForType<OnePublicMethod>();
+			It should_return_no_mappings = () => mapping.ShouldBeEmpty();
+		}
 
-	public class When_passed_a_type_with_one_public_write_only_property : ColumnMapperSpecBase<OnePublicWriteonlyProperty>
-	{
-		Because of = () => mapping = ColumnMapper.AutomapForType<OnePublicWriteonlyProperty>();
-		It should_return_no_mappings = () => mapping.ShouldBeEmpty();
-	}
+		public class When_passed_a_type_with_one_public_write_only_property : ColumnMapperSpecBase<OnePublicWriteonlyProperty>
+		{
+			Because of = () => mapping = ColumnMapper.AutomapForType<OnePublicWriteonlyProperty>();
+			It should_return_no_mappings = () => mapping.ShouldBeEmpty();
+		}
 
-	public class When_passed_a_type_with_one_public_readonly_property : ColumnMapperSpecBase<OnePublicReadonlyProperty>
-	{
-		Because of = () => mapping = ColumnMapper.AutomapForType<OnePublicReadonlyProperty>();
+		public class When_passed_a_type_with_one_public_readonly_property : ColumnMapperSpecBase<OnePublicReadonlyProperty>
+		{
+			Because of = () => mapping = ColumnMapper.AutomapForType<OnePublicReadonlyProperty>();
 
-		It should_return_one_mapping = () => mapping.Count.ShouldEqual(1);
-		It should_have_an_index_of_zero = () => mapping.First().Index.ShouldEqual(0);
-		It should_get_property_return_type = () => mapping.First().DataType.ShouldEqual(typeof (String));
-		It should_be_named_after_the_property = () => mapping.First().Name.ShouldEqual("Test");
-		It should_return_the_property_value = () => mapping.First().GetValueFrom(new OnePublicReadonlyProperty("omg")).ShouldEqual("omg");
-	}
+			It should_return_one_mapping = () => mapping.Count.ShouldEqual(1);
+			It should_have_an_index_of_zero = () => mapping.First().Index.ShouldEqual(0);
+			It should_get_property_return_type = () => mapping.First().DataType.ShouldEqual(typeof(String));
+			It should_be_named_after_the_property = () => mapping.First().Name.ShouldEqual("Test");
+			It should_return_the_property_value = () => mapping.First().GetValueFrom(new OnePublicReadonlyProperty("omg")).ShouldEqual("omg");
+		}
 
-	public class When_passed_a_type_with_two_public_readonly_properties : ColumnMapperSpecBase<TwoPublicReadonlyProperties>
-	{
-		Because of = () => mapping = ColumnMapper.AutomapForType<TwoPublicReadonlyProperties>();
-		It should_return_two_mappings = () => mapping.Count.ShouldEqual(2);
+		public class When_passed_a_type_with_two_public_readonly_properties : ColumnMapperSpecBase<TwoPublicReadonlyProperties>
+		{
+			Because of = () => mapping = ColumnMapper.AutomapForType<TwoPublicReadonlyProperties>();
+			It should_return_two_mappings = () => mapping.Count.ShouldEqual(2);
+		}
 	}
 }
--- ViewWeaver.Specs/Helpers/GridPopulation/ColumnMapperSpecs/MapForTypeSpecs.cs
+++ ViewWeaver.Specs/Helpers/GridPopulation/ColumnMapperSpecs/MapForTypeSpecs.cs
@@ -1,7 +1,36 @@
+using System;
+using System.Linq;
+using Machine.Specifications;
+using ViewWeaver.Helpers.GridPopulation;
+using ViewWeaver.Specs.TestData.Classes;
+
 namespace ViewWeaver.Specs.Helpers.GridPopulation.ColumnMapperSpecs
 {
-	public class MapForTypeSpecs
+	namespace MapForTypeSpecs
 	{
-		
+
+		public class When_passed_no_parameters : ColumnMapperSpecBase<OnePublicProperty>
+		{
+			Because of = () => mapping = ColumnMapper.ForType<OnePublicProperty>();
+
+			It should_return_an_empty_collection = () => mapping.ShouldBeEmpty();
+		}
+
+		public class When_a_single_column_is_configured : ColumnMapperSpecBase<OnePublicProperty>
+		{
+			Because of = () => mapping = ColumnMapper.ForType<OnePublicProperty>(x => x.Named("Test"));
+
+			It should_return_one_mapping = () => mapping.Count.ShouldEqual(1);
+		}
+
+		public class When_two_columns_are_configured : ColumnMapperSpecBase<OnePublicProperty>
+		{
+			Because of = () => mapping = ColumnMapper.ForType<OnePublicProperty>(x => x.Named("first"),
+																				 x => x.Named("second"));
+
+			It should_return_two_mappings = () => mapping.Count.ShouldEqual(2);
+			It should_have_the_first_configuration_first = () => mapping.First().Name.ShouldEqual("first");
+			It should_have_the_second_configuration_last = () => mapping.Last().Name.ShouldEqual("second");
+		}
 	}
 }
