7d424a4a2701cb2dd4d7417da5ef6e178f3bee13
Andy <=>
2011-12-19 20:32:49 +0000

    added configureColumns specs

---
+++ ViewWeaver.Specs/Helpers/GridPopulation/GridHandlerImplSpecs/ConfigureColumns.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Windows.Forms;
+using Machine.Specifications;
+using Moq;
+using ViewWeaver.Helpers.GridPopulation;
+using It = Machine.Specifications.It;
+
+namespace ViewWeaver.Specs.Helpers.GridPopulation.GridHandlerImplSpecs
+{
+	namespace ConfigureColumns
+	{
+		public class ConfigureColumnsSpecBase : GridHandlerImplSpecBase
+		{
+			protected static Mock<IGridPopulator> populator;
+			protected static ColumnMappingCollection<String> mapping;
+			protected static Control grid;
+
+
+			Establish context = () =>
+			{
+				populator = new Mock<IGridPopulator>();
+
+				handler = new GridHandlerImpl();
+				handler.AddPopulator(typeof(Control), populator.Object);
+
+				mapping = Mock.Of<ColumnMappingCollection<String>>();
+				grid = new Control();
+			};
+		}
+
+		public class When_passed_a_null_grid : ConfigureColumnsSpecBase
+		{
+			Because of = () => ex = Catch.Exception(() => handler.ConfigureColumns(null, mapping));
+
+			It should_throw_an_argument_null_exception = () => ex.ShouldBeOfType<ArgumentNullException>();
+		}
+
+		public class When_passed_a_null_mapping : ConfigureColumnsSpecBase
+		{
+			Because of = () => ex = Catch.Exception(() => handler.ConfigureColumns<String>(grid, null));
+
+			It should_throw_an_argument_null_exception = () => ex.ShouldBeOfType<ArgumentNullException>();
+		}
+
+		public class When_called_for_a_grid_without_a_populator : GridHandlerImplSpecBase
+		{
+			Establish context = () => handler = new GridHandlerImpl();
+			Because of = () => ex = Catch.Exception(() => handler.ConfigureColumns(new Control(), Mock.Of<ColumnMappingCollection<String>>()));
+
+			It should_throw_a_missing_populator_exception = () => ex.ShouldBeOfType<MissingPopulatorException>();
+		}
+
+		public class When_passed_an_empty_mapping : ConfigureColumnsSpecBase
+		{
+			Because of = () => handler.ConfigureColumns(grid, mapping);
+
+			It should_call_clear_columns = () => populator.Verify(x => x.ClearColumns(Moq.It.IsAny<Control>()), 
+																  Times.Once());
+
+			It should_not_call_add_column = () => populator.Verify(x => x.AddColumn(Moq.It.IsAny<Control>(), Moq.It.IsAny<ColumnMapping<String>>()), 
+																   Times.Never());
+		}
+
+		public class When_passed_a_single_mapping : ConfigureColumnsSpecBase
+		{
+			Establish context = () => mapping.Add(new ColumnMapping<String>());
+			Because of = () => handler.ConfigureColumns(grid, mapping);
+
+			It should_call_add_column_once = () => populator.Verify(x => x.AddColumn(Moq.It.IsAny<Control>(), 
+																					 Moq.It.IsAny<ColumnMapping<String>>()), 
+																   Times.Once());
+		}
+	}
+}
